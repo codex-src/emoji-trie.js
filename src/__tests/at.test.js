a1a20426258f5d04815cc21306edbb618affe4ce
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-02-03 22:21:36 +0900

    Added basic reverse test case (smileys)

--- src/__tests/at.test.js
+++ src/__tests/at.test.js
@@ -3,8 +3,8 @@ import {
 	atStart,
 } from "../at"
 
-// Creates a new string iterator; iterates one emoji or byte
-// at a time.
+// Creates a new string iterator; iterates up to one emoji
+// or byte at a time.
 function newIterator(str) {
 	let index = 0
 	const iter = () => {
@@ -15,10 +15,30 @@ function newIterator(str) {
 	return iter
 }
 
-test("smileys", () => {
+// Creates a new reverse string iterator; iterates up to one
+// emoji or byte at a time.
+function newReverseIterator(str) {
+	let index = 0
+	const iter = () => {
+		const emoji = atEnd(str.slice(0, str.length - index))
+		index += emoji.length || 1
+		return emoji
+	}
+	return iter
+}
+
+test("smileys (1 of 2)", () => {
 	const smileys = "😀😃😄"
 	const iter = newIterator(smileys)
 	expect(iter()).toBe("😀")
 	expect(iter()).toBe("😃")
 	expect(iter()).toBe("😄")
+})
+
+test("smileys (2 of 2)", () => {
+	const smileys = "😀😃😄"
+	const iter = newReverseIterator(smileys)
+	expect(iter()).toBe("😄")
+	expect(iter()).toBe("😃")
+	expect(iter()).toBe("😀")
 })
